1a67d1fc5d0b15846bd5d02b87617f41832aac96
Lucas Macedo <lucasamacedo@hotmail.com>
2022-11-21 12:01:12 -0300

    feat: add delete transation

--- src/server.js
+++ src/server.js
@@ -35,6 +35,13 @@ const transationSchema = joi.object({
     type: joi.string().required()
 })
 
+const transationPutSchema = joi.object({
+    value: joi.number().required(),
+    description: joi.string().required(),
+    type: joi.string().required(),
+    date: joi.number().required()
+})
+
 app.post ('/sign-up', async (req, res) => {
     const validation = signUpSchema.validate(req.body,{abortEarly: false})
     const user = req.body;
@@ -48,7 +55,9 @@ app.post ('/sign-up', async (req, res) => {
     try{
         const passwordHash = bcrypt.hashSync(user.password, 10);
 
-        const userExisist = await db.collection('users').findOne({...user, password: passwordHash})
+        const userExisist = await db
+            .collection('users')
+            .findOne({...user, password: passwordHash})
         if(!userExisist)
             await db.collection('users').insertOne({...user, password: passwordHash})
         else{
@@ -113,7 +122,11 @@ app.post ('/transation', async (req, res) => {
         const userExisist = await db.collection('sessions').findOne({token: token})
         
         if (userExisist){
-            await db.collection('transation').insertOne({...data,userId: userExisist.userId})
+            await db.collection('transation').insertOne({
+                ...data,
+                userId: userExisist.userId,
+                date: Date.now()
+            })
         }else{
             res.status(401).send({message:'Token inválido!'})
         }
@@ -122,6 +135,53 @@ app.post ('/transation', async (req, res) => {
         res.sendStatus(500)
     }
     res.sendStatus(201)
+})
+
+
+app.get ('/transations', async (req, res) => {
+    const token = req.headers.authorization.replace('Bearer ','')
+
+    try{
+        const userExisist = await db.collection('sessions').findOne({token: token})
+        
+        if (userExisist){
+            const transations = await db
+                .collection('transation')
+                .find({userId: userExisist.userId}).toArray()
+            transations.forEach((item)=>delete item.userId)
+            res.send(transations)
+        }else{
+            res.status(401).send({message:'Token inválido!'})
+        }
+
+    } catch(err){
+        res.sendStatus(500)
+    }
+})
+
+app.delete ('/transations/:id', async (req, res) => {
+    const id = req.params.id
+    const user = req.headers.user
+
+    try{
+        const message = await db.collection('transation').findOne({_id: ObjectId(id)})
+
+        if (message){
+            if(user===message.from){
+                await db.collection('transation').deleteOne({_id: ObjectId(id)})
+                res.status(200).send({ message: "Transacao apagada com sucesso!" });
+            }else{
+                res.sendStatus(401)
+            }
+        }else{
+            res.sendStatus(404)
+        }
+
+    } catch (err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+
 })
 
 app.listen(5000);
